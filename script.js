ce4942625e4cd727eb378f076e87d7d03a954bb0
u0028117781-svg <u0028117781@gmail.com>
2025-11-18 14:53:22 +0100

    Add HTML structure for presentation page

---
+++ script.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <title>Página de Presentación</title>
+  <link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+  <h1>Oferta Especial</h1>
+  <div class="section">
+    <p>Consigue nuestro producto con un descuento exclusivo.</p>
+  </div>
+
+  <script src="script.js"></script>
+</body>
+</html>
